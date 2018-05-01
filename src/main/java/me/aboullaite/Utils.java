1261818fa55060e0baf7bbe689bc9ccbc4920727
Biju Philip <bijuphilip0@gmail.com>
2018-04-30 22:36:15 -0500

    typo, made var final, simplified charAt call

--- src/main/java/me/aboullaite/Utils.java
+++ src/main/java/me/aboullaite/Utils.java
@@ -3,34 +3,33 @@ package me.aboullaite;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
-import java.util.stream.IntStream;
 
-/**
- * Created by aboullaite on 2017-02-23.
- */
 @Component
 public class Utils {
 
-    private Random random;
+    private final Random random;
 
-public Utils(){
-    random = new Random();
-}
+    public Utils() {
+
+        random = new Random();
+    }
 
     public String generateRandomChars(String pattern, int length) {
+
         StringBuilder sb = new StringBuilder();
 
 
-       return random.ints(0, pattern.length())
-                .mapToObj(i -> pattern.charAt(i))
+        return random.ints(0, pattern.length())
+                .mapToObj(pattern::charAt)
                 .limit(length)
                 .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
                 .toString();
     }
 
-    public Integer generateRandonInteger(Integer integer){
-    return random.ints(integer, 80)
-            .findAny()
-            .getAsInt();
+    public Integer generateRandomInteger(Integer integer) {
+
+        return random.ints(integer, 80)
+                .findAny()
+                .getAsInt();
     }
 }
