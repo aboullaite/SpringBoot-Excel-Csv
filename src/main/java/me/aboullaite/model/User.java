aab0f203268702fc3e58ee57fe032acd2aa6d916
Biju Philip <bijuphilip0@gmail.com>
2018-04-30 22:30:02 -0500

    corrected age property; consider using lombok

--- src/main/java/me/aboullaite/model/User.java
+++ src/main/java/me/aboullaite/model/User.java
@@ -2,30 +2,38 @@ package me.aboullaite.model;
 
 import org.springframework.stereotype.Component;
 
-/**
- * Created by aboullaite on 2017-02-23.
- */
-
 // In a real world app, this should be an Entity!
 @Component
 public class User {
+
     private String firstName;
+
     private String lastName;
-    private Integer   Age;
+
+    private Integer age;
+
     private String jobTitle;
+
     private String company;
+
     private String address;
+
     private String city;
+
     private String country;
+
     private String phoneNumber;
 
     public User() {
+
     }
 
-    public User(String firstName, String lastName, Integer age, String jobTitle, String company, String address, String city, String country, String phoneNumber) {
+    public User(String firstName, String lastName, Integer age, String jobTitle, String company, String address,
+                String city, String country, String phoneNumber) {
+
         this.firstName = firstName;
         this.lastName = lastName;
-        Age = age;
+        this.age = age;
         this.jobTitle = jobTitle;
         this.company = company;
         this.address = address;
@@ -35,78 +43,97 @@ public class User {
     }
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public Integer getAge() {
-        return Age;
+
+        return age;
     }
 
     public void setAge(Integer age) {
-        Age = age;
+
+        this.age = age;
     }
 
     public String getJobTitle() {
+
         return jobTitle;
     }
 
     public void setJobTitle(String jobTitle) {
+
         this.jobTitle = jobTitle;
     }
 
     public String getCompany() {
+
         return company;
     }
 
     public void setCompany(String company) {
+
         this.company = company;
     }
 
     public String getAddress() {
+
         return address;
     }
 
     public void setAddress(String address) {
+
         this.address = address;
     }
 
     public String getCity() {
+
         return city;
     }
 
     public void setCity(String city) {
+
         this.city = city;
     }
 
     public String getCountry() {
+
         return country;
     }
 
     public void setCountry(String country) {
+
         this.country = country;
     }
 
     public String getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
+
         this.phoneNumber = phoneNumber;
     }
 
     public int getColumnCount() {
+
         return getClass().getDeclaredFields().length;
     }
 }
