9cf58856fa9cc3102eaf4e2e53bfa37082a1c961
Biju Philip <bijuphilip0@gmail.com>
2018-04-30 22:35:31 -0500

    typo

--- src/main/java/me/aboullaite/service/UserService.java
+++ src/main/java/me/aboullaite/service/UserService.java
@@ -5,32 +5,30 @@ import me.aboullaite.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-/**
- * Created by aboullaite on 2017-02-23.
- */
 
 @Service
 public class UserService {
 
     private final String candidateChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     private final String candidateNum = "0123456789";
 
-
     @Autowired
-    Utils appUtil;
-    public List<User> findAllUsers(){
+    private Utils appUtil;
+
+    public List<User> findAllUsers() {
 
 
-        return IntStream.rangeClosed(1,20)
-                .mapToObj( i -> new User(appUtil.generateRandomChars(candidateChars,10), appUtil.generateRandomChars(candidateChars, 10), appUtil.generateRandonInteger(i),
-                        appUtil.generateRandomChars(candidateChars,15), appUtil.generateRandomChars(candidateChars,15), appUtil.generateRandomChars(candidateChars,20),
-                        appUtil.generateRandomChars(candidateChars,10), appUtil.generateRandomChars(candidateChars,10), appUtil.generateRandomChars(candidateNum,10)))
+        return IntStream.rangeClosed(1, 20)
+                .mapToObj(i -> new User(appUtil.generateRandomChars(candidateChars, 10), appUtil.generateRandomChars
+                        (candidateChars, 10), appUtil.generateRandomInteger(i),
+                        appUtil.generateRandomChars(candidateChars, 15), appUtil.generateRandomChars(candidateChars,
+                        15), appUtil.generateRandomChars(candidateChars, 20),
+                        appUtil.generateRandomChars(candidateChars, 10), appUtil.generateRandomChars(candidateChars,
+                        10), appUtil.generateRandomChars(candidateNum, 10)))
                 .collect(Collectors.toList());
 
     }
