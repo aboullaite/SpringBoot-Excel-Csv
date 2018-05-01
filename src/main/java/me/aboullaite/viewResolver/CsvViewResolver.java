21e087a2cbea10b574bddbfe6c208f06f51bb3b1
Biju Philip <bijuphilip0@gmail.com>
2018-04-30 22:37:12 -0500

    less is more

--- src/main/java/me/aboullaite/viewResolver/CsvViewResolver.java
+++ src/main/java/me/aboullaite/viewResolver/CsvViewResolver.java
@@ -6,10 +6,8 @@ import org.springframework.web.servlet.ViewResolver;
 
 import java.util.Locale;
 
-/**
- * Created by aboullaite on 2017-02-24.
- */
 public class CsvViewResolver implements ViewResolver {
+
     @Override
     public View resolveViewName(String s, Locale locale) throws Exception {
 
