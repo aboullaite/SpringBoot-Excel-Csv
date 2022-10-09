82d6a6b36e8575e22891a3bf14e646b1e7567343
darren <darren94me@gmail.com>
2022-10-09 17:11:04 +0800

    header columns can modify

--- src/main/java/me/aboullaite/controller/ExportController.java
+++ src/main/java/me/aboullaite/controller/ExportController.java
@@ -5,13 +5,8 @@ import me.aboullaite.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class ExportController {
